837b15523785b03e4851f5e21ca5f2522d5c6670
Julia <schmid.julia96@gmail.com>
2023-03-17 08:05:02 +0200

    experiment with outlier handling

--- pandora/pca.py
+++ pandora/pca.py
@@ -125,6 +125,9 @@ def _clip_missing_samples_for_comparison(pca1: PCA, pca2: PCA) -> Tuple[PCA, PCA
 
     pca1 = _correct_for_missing_samples(pca1, in_both)
     pca2 = _correct_for_missing_samples(pca2, in_both)
+
+    assert pca1.pc_vectors.shape == pca2.pc_vectors.shape
+
     return pca1, pca2
 
 
